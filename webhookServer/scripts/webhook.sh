27da53fd89b9813fc293eeb6a859ac35f9c703e9
Forest <yiqing.zhang@bl.com>
2018-05-20 15:36:08 +0800

    update

---
+++ webhookServer/scripts/webhook.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "=========== BEGIN fzwebhook =========="
+
+
+git add -A
+git pull
+
+# 需要到各个文件夹下去npm install
+cd ../client
+npm install
+
+cd ../server
+npm install
+
+pm2 restart nas_demo
+
+echo "======== END webhook =========="
