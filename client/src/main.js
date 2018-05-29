0cf9889f7525f909dba4efbe3916d4175c426fd8
Forest <yiqing.zhang@bl.com>
2018-05-29 14:20:47 +0800

    update

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,85 @@
-import Vue from "vue";
-import App from "./App";
-import router from "./router/index";
+/* ============
+ * Main File
+ * ============
+ *
+ * Will initialize the application.
+ */
 
-import PaperDashboard from "./plugins/paperDashboard";
-import "vue-notifyjs/themes/default.css";
+import Vue from 'vue';
 
-Vue.use(PaperDashboard);
+/* ============
+ * Plugins
+ * ============
+ *
+ * Import and bootstrap the plugins.
+ */
+
+import './plugins/vuex';
+import './plugins/axios';
+import { i18n } from './plugins/vue-i18n';
+import { router } from './plugins/vue-router';
+import './plugins/vuex-router-sync';
+import './plugins/bootstrap';
+import './plugins/font-awesome';
+
+/* ============
+ * Styling
+ * ============
+ *
+ * Import the application styling.
+ * Stylus is used for this boilerplate.
+ *
+ * If you don't want to use Stylus, that's fine!
+ * Replace the stylus directory with the CSS preprocessor you want.
+ * Import the entry point here & install the webpack loader.
+ *
+ * It's that easy...
+ *
+ * http://stylus-lang.com/
+ */
+
+import './assets/stylus/app.styl';
+
+/* ============
+ * Main App
+ * ============
+ *
+ * Last but not least, we import the main application.
+ */
+
+import App from './App';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+store.dispatch('auth/check');
 
 /* eslint-disable no-new */
 new Vue({
+  /**
+   * Bind the Vue instance to the HTML.
+   */
+  el: '#app',
+
+  /**
+   * The localization plugin.
+   */
+  i18n,
+
+  /**
+   * The router.
+   */
   router,
-  render: h => h(App)
-}).$mount("#app");
+
+  /**
+   * The Vuex store.
+   */
+  store,
+
+  /**
+   * Will render the application.
+   *
+   * @param {Function} h Will create an element.
+   */
+  render: h => h(App),
+});
--- client/src/pages/SuperDict.vue
+++ client/src/pages/SuperDict.vue
@@ -1,234 +1,103 @@
 <template>
-  <div>
-
-    <div class="logo">
-      <div class="name">SUPER DICTIONARY</div>
-      <div class="img logo_rotate">
-        <img src="img/logo.png" alt="">
+  <v-layout>
+    <v-card contextual-style="dark">
+      <span slot="header">
+        Welcome 222!
+      </span>
+      <div slot="body">
+        <p>Nas Demo 项目</p>
+        <div>
+          <button type="button" class="btn btn-success" @click="onSearch">搜索</button>
+          <button type="button" class="btn btn-info" @click="onSubmit">上传</button>
+        </div>
+        <br>
+        <div>
+          <ul class="list-group">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+          </ul>
+        </div>
       </div>
-    </div>
-    <div class="noExtension hide" id="noExtension">
-      NOTE: Please install <a target="_blank" href="https://github.com/ChengOrangeJu/WebExtensionWallet">WebExtensionWallet</a>  to use SUPER DICTIONARY
-    </div>
-    <div class="search">
-      <input id="search_value" type="text">
-      <button @click="onSubmit">submit</button>
-    </div>
-    <div class="search">
-
-      <button @click="onSearch">search</button>
-    </div>
-
-    <div class="result_success hide">
-      <div id=search_banner></div>
-      <p id=search_result> wait for content </p>
-      <div class="author">
-        <i><p> Author:</p> <p id=search_result_author> dasdajkajksdhjasdkjahdkjad</p></i>
-      </div>
-    </div>
-
-    <div class="result_faile hide">
-      Failed to find related information. Do you want to <button id="add">add</button> infromation for "<i id="result_faile_add">asd</i>"?
-    </div>
-
-    <div class="add_banner hide">
-      <input type="text" id="add_value" placeholder="input contents for your keyword">
-      <button @click="onSubmit">submit</button>
-    </div>
-
-  </div>
+      <div slot="footer"></div>
+    </v-card>
+  </v-layout>
 </template>
 <script>
 
-import NebPay from 'nebpay.js'
-import nebulas from 'nebulas'
+  import NebPay from 'nebpay.js';
+  import nebulas from 'nebulas';
+  import VLayout from '@/layouts/Default';
+  import VCard from '@/components/Card';
+  export default {
+    components: {
+      VLayout,
+      VCard,
+    },
+    /**
+     * Chart data used to render stats, charts. Should be replaced with server data
+     */
+    data() {
+      return {
+        dappAddress: 'n1exFH19cJbzVDhTnb2DaoRKztZFJn4efvV',
+      };
+    },
+    methods: {
+      onSearch() {
+        console.log('search');
+        const Account = nebulas.Account;
+        const neb = new nebulas.Neb();
+        neb.setRequest(new nebulas.HttpRequest('https://testnet.nebulas.io'));
+        const from = Account.NewAccount().getAddressString();
 
-export default {
-  components: {
-//    StatsCard,
-//    ChartCard
-  },
-  /**
-   * Chart data used to render stats, charts. Should be replaced with server data
-   */
-  data() {
-    return {
-      dappAddress: 'n1exFH19cJbzVDhTnb2DaoRKztZFJn4efvV'
-    }
-  },
-  methods: {
-    onSearch() {
-      console.log('search');
-      var Account = nebulas.Account;
-      var neb = new nebulas.Neb();
-      neb.setRequest(new nebulas.HttpRequest("https://testnet.nebulas.io"));
-      var from = Account.NewAccount().getAddressString();
+        const value = '0';
+        const nonce = '0';
+        const gas_price = '1000000';
+        const gas_limit = '2000000';
+        const callFunction = 'get';
+        const callArgs = '["' + 'abc' + '"]'; // in the form of ["args"]
+        const contract = {
+          function: callFunction,
+          args: callArgs,
+        };
 
-      var value = "0";
-      var nonce = "0"
-      var gas_price = "1000000"
-      var gas_limit = "2000000"
-      var callFunction = "get";
-      var callArgs = "[\"" + 'abc'+ "\"]"; //in the form of ["args"]
-      var contract = {
-        "function": callFunction,
-        "args": callArgs
-      }
-
-      neb.api.call(from,this.dappAddress,value,nonce,gas_price,gas_limit,contract).then(function (resp) {
+        neb.api.call(from, this.dappAddress, value, nonce, gas_price, gas_limit, contract).then((resp) => {
 //        this.cbSearch(resp)
-        console.log("response of search: " + JSON.stringify(resp))
-      }).catch(function (err) {
-        //cbSearch(err)
-        console.log("error:" + err.message)
-      })
+          console.log(`response of search: ${JSON.stringify(resp)}`);
+        }).catch((err) => {
+          // cbSearch(err)
+          console.log(`error:${err.message}`);
+        });
+      },
+      onSubmit() {
+        console.log('submit');
+        const nebPay = new NebPay();
 
-    },
-    onSubmit() {
-      console.log('submit');
-      var nebPay = new NebPay();
-
-      var to = this.dappAddress;
-      var value = "0";
-      var callFunction = "save"
-      var callArgs = ["abc","def"]
+        const to = this.dappAddress;
+        const value = '0';
+        const callFunction = 'save';
+        const callArgs = ['abc', 'def'];
 //      var callArgs = "[\"" + $("#search_value").val() + "\",\"" + $("#add_value").val() + "\"]"
-      var serialNumber = nebPay.call(to, value, callFunction, callArgs, {    //使用nebpay的call接口去调用合约,
-        listener: this.cbPush        //设置listener, 处理交易返回信息
-      });
+        const serialNumber = nebPay.call(to, value, callFunction, callArgs, {    // 使用nebpay的call接口去调用合约,
+          listener: this.cbPush,        // 设置listener, 处理交易返回信息
+        });
 
-      console.log('serialnum', serialNumber);
+        console.log('serialnum', serialNumber);
+      },
+      cbPush(resp) {
+        console.log(`response of push: ${JSON.stringify(resp)}`);
+      },
+      cbSearch(resp) {
+        console.log(`response of search: ${JSON.stringify(resp)}`);
+      },
+
+
     },
-    cbPush(resp) {
-      console.log("response of push: " + JSON.stringify(resp))
-    },
-    cbSearch(resp) {
-      console.log("response of search: " + JSON.stringify(resp))
-    }
+  };
 
-
-  }
-};
 </script>
 <style>
-  /*.logo {*/
-    /*width: 60%;*/
-    /*height: 500px;*/
-    /*margin: auto;*/
-  /*}*/
 
-  /*.name {*/
-    /*text-align: center;*/
-    /*font-size: 66px;*/
-    /*text-shadow: 3px 5px grey, 1px 1px #333;*/
-  /*}*/
-
-  /*.img {*/
-    /*width: 48%;*/
-    /*height: 53%;*/
-    /*margin: auto;*/
-    /*margin-top: 38px;*/
-  /*}*/
-
-  /*.img img {*/
-    /*width: 100%;*/
-    /*height: 100%;*/
-  /*}*/
-
-  /*.search {*/
-    /*width: 80%;*/
-    /*height: 100px;*/
-    /*margin: auto;*/
-  /*}*/
-
-  /*.noExtension {*/
-    /*width: 60%;*/
-    /*height: 100px;*/
-    /*margin: auto;*/
-    /*font-size: 23px;*/
-  /*}*/
-
-  /*#search_value {*/
-    /*width: 80%;*/
-    /*height: 50px;*/
-    /*box-shadow: 3px 5px grey, 1px 1px #333;*/
-  /*}*/
-
-  /*.search button {*/
-    /*width: 18%;*/
-    /*height: 50px;*/
-    /*margin-left: 6px;*/
-    /*box-shadow: 3px 5px grey, 1px 1px #333;*/
-  /*}*/
-
-  /*@keyframes rotate {*/
-    /*0% { transform:rotateY(0deg);}*/
-    /*25% { transform:rotateY(180deg);}*/
-    /*50% { transform:rotateY(0deg);}*/
-    /*75% { transform:rotateY(180deg);}*/
-    /*100% { transform:rotateY(0deg);}*/
-  /*}*/
-
-  /*.logo_rotate {*/
-    /*!**/
-    /*animation: rotate 10s infinite;*/
-    /*animation-fill-mode: forwards;*/
-    /*animation-timing-function: linear;*/
-        /**!*/
-    /*!*  当动画结束时，让<div>元素保留上一个关键帧的样式值 *!*/
-  /*}*/
-
-  /*.result_success {*/
-    /*width: 60%;*/
-    /*margin: auto;*/
-    /*font-size: 22px*/
-  /*}*/
-
-  /*.result_faile{*/
-    /*width: 60%;*/
-    /*margin: auto;*/
-    /*font-size: 22px*/
-  /*}*/
-
-  /*.add_banner{*/
-    /*width: 80%;*/
-    /*margin: auto;*/
-  /*}*/
-
-  /*.add_banner input{*/
-    /*width: 80%;*/
-    /*height: 50px;*/
-    /*box-shadow: 3px 5px grey, 1px 1px #333;*/
-  /*}*/
-
-  /*.add_banner button{*/
-    /*width: 18%;*/
-    /*height: 50px;*/
-    /*margin-left: 6px;*/
-    /*box-shadow: 3px 5px grey, 1px 1px #333;*/
-  /*}*/
-
-  /*#search_banner{*/
-    /*font-size: 50px;*/
-    /*border-bottom: 1px solid black;*/
-  /*}*/
-
-
-
-  .hide{
-    display: none;
-  }
-
-  /*.contenner{*/
-    /*background: url("img/bg.jpg");*/
-    /*height: 1500px;*/
-  /*}*/
-
-  .author{
-    text-align: right;
-  }
-  .author p{
-    display: inline-block;
-    font-size: 18px;
-  }
 </style>
